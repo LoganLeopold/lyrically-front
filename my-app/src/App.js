72169161fc8133e58683730cd2cada076cf3f205
Logan Leopold <loganleopld@me.com>
2019-02-19 17:23:31 -0500

    front end pulling data from backend on one route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,54 @@
-
 import React, { Component } from "react";
 import "./App.css";
+import axios from 'axios'
 
 import CreateItem from "./CreateArtist/createArtist";
 import { Link, Route, Switch } from "react-router-dom";
 import ListItem from "./ListArtist/ListArtist";
-import UpdateSong from './UpdateSong/UpdateSong'
+import UpdateSong from "./UpdateSong/UpdateSong";
+import ListSong from "./ListSong/ListSong";
 
 class App extends Component {
   state = {
-    title: this.title,
-    url: this.url,
-    description: this.description
+    songs: [],
+    artists: []
   };
+
+  componentDidMount() {
+    axios.get("https://lyrically123.herokuapp.com/songs")
+        .then((res) => {
+            console.log(res)
+            this.setState({
+                songs: res.data
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <h3>  <a href="/">Home</a></h3>
+          Header
+          {/* <h3>  <a href="/">Home</a></h3>
           <Link
             to="/createArtist"
             style={{ textDecoration: "none", color: "lightblue" }}
           >
            <h3 className="new"> Create New Song</h3>
           </Link>
-           <h3><Link to="/listArtist"> List </Link></h3>
+           <h3><Link to="/listArtist"> List </Link></h3> */}
         </header>
 
         <h1>Song List!</h1>
 
         <section className="form" />
         <main>
-          <Switch>
-            <Route
+          {/* <Switch> */}
+          <Route path='/' render={(routerProps) => <ListSong {...this.state}{...routerProps} />} />
+          {/* <Route
               path="/createArtist"
               render={props => (
                 <CreateItem
@@ -42,10 +57,10 @@ class App extends Component {
                   stocks={this.props.stocks}
                 />
               )}
-            />
+            /> */}
 
-            <Route path="/listItems" Component={ListItem} />
-          </Switch>
+          {/* <Route path="/listItems" Component={ListItem} /> */}
+          {/* </Switch> */}
         </main>
       </div>
     );
---
+++ my-app/src/ListSong/ListSong.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class ListSong extends Component {
+
+    // constructor() {
+    //     super() 
+    //     this.state = {
+    //         ListArtist: []
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     axios.get("https://lyrically123.herokuapp.com/songs")
+    //         .then((res) => {
+    //             console.log(res)
+    //             this.setState({
+    //                 ListArtist: res.data
+    //             })
+    //         })
+    //         .catch((err) => {
+    //             console.log(err)
+    //         })
+    // }
+
+    render() {
+        console.log(this.props.songs)
+        let songList = this.props.songs.map ( song => {
+        
+        return (
+            <div key={song._id}>
+                <h1>{song.title}</h1>
+                <h1>{song.artist}</h1>
+                <p>{song.lyrics}</p>
+            </div>
+            
+        )
+        
+    })
+
+    console.log(songList)
+        return (
+        <div>
+        <h2>Hello</h2>
+        {songList}
+        </div>
+        )
+    }
+}
+
+export default ListSong;
