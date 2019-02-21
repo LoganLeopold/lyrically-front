08c4de7e905c1f3e59033dff0efb32eb02e15872
Logan Leopold <loganleopld@me.com>
2019-02-20 19:50:41 -0500

    trying to get create working - I need a way to tell the form values where to get lyrics from but the rendering cycle is making that difficult

---
+++ my-app/src/UpdateSong/UpdateForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class UpdateForm extends Component {
+    render() {
+        return (
+            <div>
+                  <h1>Update Song</h1>
+        {/* <form name="update" onSubmit={this.handleSubmit}>
+          <label>Lyrics:</label>
+          <input
+            className="lyricsInput"
+            type="text"
+            defaultValue={this.props.thisSong[0].lyrics}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Update Lyrics</button>
+        </form>  */}
+            </div>
+        );
+    }
+}
+
+export default UpdateForm;
--- my-app/src/UpdateSong/UpdateSong.js
+++ my-app/src/UpdateSong/UpdateSong.js
@@ -1,73 +1,107 @@
-import React, { Component } from 'react';
-import { FormControl } from "react-bootstrap"
-import { FormGroup } from "react-bootstrap"
-import { Button } from 'react-bootstrap'
-import axios from 'axios'
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { FormControl } from "react-bootstrap";
+import { FormGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+import {Route} from 'react-router-dom'
+import UpdateForm from './UpdateForm'
 
 class UpdateSong extends Component {
-    // constructor(){
-    //     super()
-    //     this.state={
-    //         songs:'',
-    //         artists:''
-    //     }
-    // }
+  constructor() {
+    super();
+    this.state = {
+      songLyrics: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.componentDidMount = this.componentDidMount.bind(this)
+  }
 
-    
-    handleSubmit = event => {
-        // this.setState({ id: event.target.value });
-        
+  // componentDidMount() {
+
+  //   this.props.songs.filter(song => {
+  //     if (song._id === this.props.match.params.id) {
+  //       this.setState({
+  //         song: song
+  //       })
+  //     }
+  //   });
+  // }
+
+  handleChange = event => {
+    this.setState({
+      songLyrics: event.target.value
+    });
+  };
+
+  handleSubmit = (event) => {
+    console.log(this.props.params.match.id);
+    event.preventDefault();
+    // axios.post(`https://lyrically123.herokuapp.com/songs/${this.props.params.match._id}`, {lyrics: this.state.songLyrics})
+    // .then (res => {
+    //   console.log(res.data)
+    // })
+    // .then (() => {
+    //   return <Redirect to='/songs' />
+    // })
+  };
+
+  render() {
+    let thisSong = [];
+
+    this.props.songs.filter(song => {
+      if (song._id === this.props.match.params.id) {
+        thisSong.push(song);
+        console.log(song.lyrics)
       }
-    
-      render() {
-        return (
-          
-          <div>
-            <h1>Update Song</h1>
-            <form name='update' onSubmit={this.handleSubmit}>
-              <label>
-                Person ID:
-              </label>
-              <input type="text" name="id" onChange={this.handleChange} />
-              <button type="submit">Delete</button>
-            </form>
+    });
 
-            <form>
-            <button type='submit' name={this.props.match.params.id} onClick={this.props.delete}>Delete this Note</button>
-            </form>
+    return (
+      <div>
+        <UpdateForm thisSong={thisSong} {...this.props} {...this.state} />
 
-          </div>
-        )
-      }
-    
-// upDateDidMount
-//     render() {
-//         return (
-//             <div className="UpdateSong">
-//                 <form>
-//                     <FormGroup bsSize="large">
-//                         <label>Song</label>
-//                         <FormControl type="text" placeholder="Song" />
-//                     </FormGroup>
-//                     <FormGroup bsSize="large">
-//                         <label>Title</label>
-//                         <FormControl type="text" placeholder="Title" />
-//                         <FormGroup bsSize="large">
-//                         <label>Lyrics</label>
-//                         <FormControl type="text" placeholder="Lyric" />
-//                         <p>
-//                             <Button className="button" bsStyle="primary">update</Button>
-//                         </p>
-//                     </FormGroup>
-//                     {/* <FormGroup bsSize="large">
-//                         <label>Add to your Artist</label>
-//                         <FormControl type="text" placeholder="Item or Task" />
-//                         */}
-//                     </FormGroup>
-//                 </form>
-//             </div>
-//         );
-//     }
+        <form>
+          <button
+            type="submit"
+            name={this.props.match.params.id}
+            onClick={this.props.delete}
+          >
+            Delete this Note
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  // upDateDidMount
+  //     render() {
+  //         return (
+  //             <div className="UpdateSong">
+  //                 <form>
+  //                     <FormGroup bsSize="large">
+  //                         <label>Song</label>
+  //                         <FormControl type="text" placeholder="Song" />
+  //                     </FormGroup>
+  //                     <FormGroup bsSize="large">
+  //                         <label>Title</label>
+  //                         <FormControl type="text" placeholder="Title" />
+  //                         <FormGroup bsSize="large">
+  //                         <label>Lyrics</label>
+  //                         <FormControl type="text" placeholder="Lyric" />
+  //                         <p>
+  //                             <Button className="button" bsStyle="primary">update</Button>
+  //                         </p>
+  //                     </FormGroup>
+  //                     {/* <FormGroup bsSize="large">
+  //                         <label>Add to your Artist</label>
+  //                         <FormControl type="text" placeholder="Item or Task" />
+  //                         */}
+  //                     </FormGroup>
+  //                 </form>
+  //             </div>
+  //         );
+  //     }
 }
 
 export default UpdateSong;
