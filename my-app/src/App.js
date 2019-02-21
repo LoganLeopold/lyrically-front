cd3b23abbd3b16f4dfd642950f4ff6257b283eae
Logan Leopold <loganleopld@me.com>
2019-02-21 13:55:10 -0500

    trying to set up create

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,7 @@ import { Link, Route, Switch } from "react-router-dom";
 import UpdateSong from "./UpdateSong/UpdateSong";
 import ListSong from "./ListSong/ListSong";
 import ListArtist from './ListArtist/ListArtist';
+import CreateSong from './CreateSong/CreateSong'
 
 class App extends Component {
   constructor() {
@@ -56,11 +57,19 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Lyrically</h1>
-
+        <nav>
+          <Link to={'/songs'}>Songs</Link>
+          <br></br>
+          <Link to={'/artists'}>Artists</Link>
+        </nav>        
         <main>
           <Route path="/songs" exact render={routerProps => <ListSong delete={this.delete} {...this.state} {...routerProps} />}/>
-          <Route path="/artists" exact render={routerProps => <ListArtist {...this.state} {...routerProps} />}/>
           <Route path="/songs/:id" exact render={routerProps => <UpdateSong delete={this.delete} handleChange={this.handleChange} {...routerProps} {...this.state}/>}/>
+          <Route path="/songs" exact render={routerProps => <CreateSong delete={this.delete} handleChange={this.handleChange} {...routerProps} {...this.state}/>}/>
+
+          <Route path="/artists" exact render={routerProps => <ListArtist {...this.state} {...routerProps} />}/>
+          <Route path="/artists/:id" exact render={routerProps => <UpdateSong delete={this.delete} handleChange={this.handleChange} {...routerProps} {...this.state}/>}/>
+
         </main>
 
       </div>
